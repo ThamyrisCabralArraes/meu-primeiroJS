438d3fa60ddc5077dfbf1119d396c693a4a846dc
ThamryisArraes <thamyriscabral@hotmail.com>
2022-10-11 07:57:34 -0300

    first commit

--- meuscript.js
+++
@@ -1,8 +0,0 @@
-const nome = prompt('Digite o seu nome:')
-const idade = prompt('Digite sua idade:')
-// document.write('<h1>Olá ' + nome + ', tudo bem? Estou vendo aqui que você tem ' + idade + ' anos.</h1>')
-const divi = document.getElementById('divi')
-const h1 = document.createElement('h1')
-
-h1.innerText = ('Olá ' + nome + ', tudo bem? Estou vendo aqui que você tem ' + idade + ' anos.')
-divi.appendChild(h1)
